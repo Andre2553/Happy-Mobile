6f67c33ae9e86b77ab8946626fed78d784d3edcd
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2020-10-20 11:46:23 +1100

    Git init

---
+++ src/routes.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const {Navigator, Screen} = createStackNavigator();
+
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+import SuccessRegistration from './pages/CreateOrphanage/SuccessRegistration';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
+
+export default function Routes(){
+  return(
+    <NavigationContainer>
+      <Navigator 
+        screenOptions={{ 
+          headerShown: false, 
+          cardStyle: { 
+            backgroundColor: '#F2F3F5' 
+          } 
+        }}
+      >
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap} 
+        />
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails} 
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orphanages" />
+          }}
+        />
+
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition} 
+          options={{
+            headerShown: true,
+            header: () => <Header title="Select on the map" />
+          }}
+        />
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData} 
+          options={{
+            headerShown: true,
+            header: () => <Header title="Give the information" />
+          }}
+        />
+
+        <Screen 
+          name="SuccessRegistration" 
+          component={SuccessRegistration} 
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
